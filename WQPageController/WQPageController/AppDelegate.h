8bf69755b72e97227801a6374564c7707ea10f13
isbase <isbase@users.noreply.github.com>
2014-07-11 15:07:05 +0800

    new add
    new add

---
+++ WQPageController/WQPageController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WQPageController
+//
+//  Created by dev on 14-7-11.
+//  Copyright (c) 2014年 Today. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
